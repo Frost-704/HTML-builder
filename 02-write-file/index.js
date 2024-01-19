98d7301e090f074cbc25ccba352a47feacd8b713
Frost-704 <boolean.pg.me@gmail.com>
2024-01-20 00:31:02 +0300

    feat: finish 02-write-task

---
+++ 02-write-file/index.js
@@ -0,0 +1,25 @@
+const readline = require('readline');
+const path = require('path');
+const fs = require('fs');
+let rl = readline.createInterface(process.stdin);
+console.log('Hello, please add a text');
+rl.on('line', (input) => {
+  if (input.trim() === 'exit') {
+    rl.close();
+  } else {
+    fs.writeFile(
+      path.resolve(`${__dirname}`, 'text.txt'),
+      input,
+      { flag: 'a' },
+      (err) => {
+        if (err) {
+          console.error('Error:', err);
+        }
+      },
+    );
+  }
+});
+process.on('SIGINT', () => rl.close());
+rl.on('close', () => {
+  console.log('Goodbye!');
+});
