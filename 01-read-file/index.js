005e0459b41f9b69ff72a7b47afe9aec809c5e72
Frost-704 <boolean.pg.me@gmail.com>
2024-01-19 22:02:37 +0300

    feat: finish 01-read-file task

---
+++ 01-read-file/index.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+const input = fs.createReadStream(
+  path.resolve(`${__dirname}`, 'text.txt'),
+  'utf-8',
+);
+input.on('data', (chunk) => console.log(chunk));
+input.on('error', (error) => console.log('Error', error.message));
