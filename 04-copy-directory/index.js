2b23666d1d5f5a330aadd15baad58e1b81a1d526
Frost-704 <boolean.pg.me@gmail.com>
2024-01-20 19:12:06 +0300

    fix: fix copyDir function

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,13 +2,14 @@ const { rm, copyFile, readdir, mkdir } = require('fs/promises');
 const path = require('path');
 
 async function copyDir() {
+  await mkdir(path.join(`${__dirname}`, 'files-copy'), { recursive: true });
   const originalPath = path.join(`${__dirname}`, 'files');
   const copyPath = path.join(`${__dirname}`, 'files-copy');
   await rm(copyPath, { recursive: true });
   await mkdir(path.join(`${__dirname}`, 'files-copy'), { recursive: true });
   const files = await readdir(originalPath);
   for (const file of files) {
-    copyFile(`${originalPath}/${file}`, `${copyPath}/${file}`);
+    await copyFile(`${originalPath}/${file}`, `${copyPath}/${file}`);
   }
 }
 copyDir();
